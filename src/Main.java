61294dc25cf2d3fd9c21d2afc410cd318e146040
SergioK1 <SergeyK89@mail.ru>
2021-10-05 19:11:05 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 20_000;
+        int pricePerMile = 20;
+        int numberMiles = ticketPrice / pricePerMile;
+
+        System.out.println(numberMiles + "Миль");
+    }
+}
